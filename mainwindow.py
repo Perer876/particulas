e52e4ff8a8db788d634fb7c02000d28b61e49996
Oscar Arámbula <oscar.arambula4388@alumnos.udg.mx>
2020-10-22 01:32:37 -0500

    Interfaz funcional que administra particulas

--- mainwindow.py
+++ mainwindow.py
@@ -1,14 +1,64 @@
 from PySide2.QtWidgets import QMainWindow
 from PySide2.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
+from particulas import Particulas
+from particula import Particula
+from algoritmos import distancia_euclidiana
 
 class MainWindow(QMainWindow):
     def __init__(self):
        super(MainWindow, self).__init__()
-       ui = Ui_MainWindow()
-       ui.setupUi(self)
-       ui.pushButton.clicked.connect(self.click_agregar)
+       self.__particulas = Particulas()
+       self.ui = Ui_MainWindow()
+       self.ui.setupUi(self)
+       self.ui.agregarFinal_pushButton.clicked.connect(self.click_agregarFinal)
+       self.ui.agregarInicio_pushButton.clicked.connect(self.click_agregarInicio)
+       self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
+    
+    @Slot()
+    def click_mostrar(self):
+        self.ui.salida.clear()
+        self.ui.salida.insertPlainText(str(self.__particulas))
+        # self.__particulas.mostrar()
 
     @Slot()
-    def click_agregar(self):
-        print('hiciste click en agregar')
+    def click_agregarFinal(self):
+        if self.ui.id_lineEdit.text() == '':
+            iD = 0
+        else:
+            iD = int(self.ui.id_lineEdit.text())
+        origenX = self.ui.origenX_spinBox.value()
+        origenY = self.ui.origenY_spinBox.value()
+        destinoX = self.ui.destinoX_spinBox.value()
+        destinoY = self.ui.destinoY_spinBox.value()
+        if self.ui.velocidad_lineEdit.text() == '':
+            velocidad = 0
+        else:
+            velocidad = int(self.ui.id_lineEdit.text())
+        red = self.ui.red_spinBox.value()
+        green = self.ui.green_spinBox.value()
+        blue = self.ui.blue_spinBox.value()
+
+        particula = Particula(iD, origenX, origenY, destinoX, destinoY, velocidad, red, green, blue)
+        self.__particulas.agregar_final(particula)
+
+    @Slot()
+    def click_agregarInicio(self):
+        if self.ui.id_lineEdit.text() == '':
+            iD = 0
+        else:
+            iD = int(self.ui.id_lineEdit.text())
+        origenX = self.ui.origenX_spinBox.value()
+        origenY = self.ui.origenY_spinBox.value()
+        destinoX = self.ui.destinoX_spinBox.value()
+        destinoY = self.ui.destinoY_spinBox.value()
+        if self.ui.velocidad_lineEdit.text() == '':
+            velocidad = 0
+        else:
+            velocidad = int(self.ui.id_lineEdit.text())
+        red = self.ui.red_spinBox.value()
+        green = self.ui.green_spinBox.value()
+        blue = self.ui.blue_spinBox.value()
+
+        particula = Particula(iD, origenX, origenY, destinoX, destinoY, velocidad, red, green, blue)
+        self.__particulas.agregar_inicio(particula)
