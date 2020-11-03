352923203a49ec6f849a684fb90d68160f6cc7c3
Oscar Arámbula <oscar.arambula4388@alumnos.udg.mx>
2020-11-03 13:49:31 -0600

    Pestaña funcional de tabla, con unos ajustes de las funciones antiguas más un archivo de datos

--- mainwindow.py
+++ mainwindow.py
@@ -1,4 +1,4 @@
-from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox
+from PySide2.QtWidgets import QMainWindow, QFileDialog, QMessageBox, QTableWidgetItem
 from PySide2.QtCore import Slot
 from ui_mainwindow import Ui_MainWindow
 from particulas import Particulas
@@ -16,11 +16,101 @@ class MainWindow(QMainWindow):
        self.ui.agregarFinal_pushButton.clicked.connect(self.click_agregarFinal)
        self.ui.agregarInicio_pushButton.clicked.connect(self.click_agregarInicio)
        self.ui.mostrar_pushButton.clicked.connect(self.click_mostrar)
-       self.ui.actionAbrir.triggered.connect(self.action_abrir_archivo)
-       self.ui.actionGuardar.triggered.connect(self.action_guardar_archivo)
        self.ui.buscar_pushButton.clicked.connect(self.click_buscar)
        self.ui.mostrarTabla_pushButton.clicked.connect(self.click_mostrarTabla)
+        
+       self.ui.actionAbrir.triggered.connect(self.action_abrir_archivo)
+       self.ui.actionGuardar.triggered.connect(self.action_guardar_archivo)
     
+    # Inserta un objeto Particula en la fila indicada.
+    def insertar_particula_en_tabla(self, particula:Particula, row:int):
+        id_widget = QTableWidgetItem(str(particula.id))
+        origen_x_widget = QTableWidgetItem(str(particula.origen_x))
+        origen_y_widget = QTableWidgetItem(str(particula.origen_y))
+        destino_x_widget = QTableWidgetItem(str(particula.destino_x))
+        destino_y_widget = QTableWidgetItem(str(particula.destino_y))
+        velocidad_widget = QTableWidgetItem(str(particula.velocidad))
+        red_widget = QTableWidgetItem(str(particula.red))
+        green_widget = QTableWidgetItem(str(particula.green))
+        blue_widget = QTableWidgetItem(str(particula.blue))
+        distancia_widget = QTableWidgetItem(str(particula.distancia))    
+        self.ui.tabla.setItem(row, 0, id_widget)
+        self.ui.tabla.setItem(row, 1, origen_x_widget)
+        self.ui.tabla.setItem(row, 2, origen_y_widget)
+        self.ui.tabla.setItem(row, 3, destino_x_widget)
+        self.ui.tabla.setItem(row, 4, destino_y_widget)
+        self.ui.tabla.setItem(row, 5, velocidad_widget)
+        self.ui.tabla.setItem(row, 6, red_widget)
+        self.ui.tabla.setItem(row, 7, green_widget)
+        self.ui.tabla.setItem(row, 8, blue_widget)
+        self.ui.tabla.setItem(row, 9, distancia_widget)
+
+    def sacar_particula(self):
+        iD = self.ui.id_lineEdit.text()
+        velocidad = self.ui.velocidad_lineEdit.text()
+        # Verificamos que las casillas libres no esten vacias y tambien que solo contengan números.
+        if (iD.isnumeric() and velocidad.isnumeric()):
+            origenX = self.ui.origenX_spinBox.value()
+            origenY = self.ui.origenY_spinBox.value()
+            destinoX = self.ui.destinoX_spinBox.value()
+            destinoY = self.ui.destinoY_spinBox.value()
+            red = self.ui.red_spinBox.value()
+            green = self.ui.green_spinBox.value()
+            blue = self.ui.blue_spinBox.value()
+            # Instanciamos una particula para solamente agregarla al final de la lista.
+            particula = Particula(int(iD), origenX, origenY, destinoX, destinoY, int(velocidad), red, green, blue)
+            return particula
+        else:
+            return None
+
+    @Slot()
+    def click_mostrar(self):
+        self.ui.salida.clear()
+        self.ui.salida.insertPlainText(str(self.__particulas))
+
+    @Slot()
+    def click_agregarFinal(self):
+        particula = self.sacar_particula()
+        if(particula != None):
+            self.__particulas.agregar_final(particula)
+
+    @Slot()
+    def click_agregarInicio(self):
+        particula = self.sacar_particula()
+        if(particula != None):
+            self.__particulas.agregar_inicio(particula)
+
+    @Slot()
+    def click_buscar(self):
+        id_particula = self.ui.buscar_lineEdit.text()
+        if(id_particula.isnumeric()):
+            id_particula = int(id_particula)
+            encontrado = False
+            for particula in self.__particulas:
+                if(particula.id == id_particula):
+                    self.ui.tabla.clear()
+                    self.ui.tabla.setRowCount(1)
+                    self.insertar_particula_en_tabla(particula, 0)
+                    encontrado = True
+                    return
+            if not encontrado:
+                QMessageBox.warning(
+                    self,
+                    "Atención",
+                    f"La partiula con el id {id_particula} no fue encontrado"
+                )
+
+    @Slot()
+    def click_mostrarTabla(self):
+        self.ui.tabla.setColumnCount(10)
+        headers = ['ID','Origen X','Origen Y','Destino X','Destino Y','Velocidad','Red','Green','Blue','Distancia']
+        self.ui.tabla.setHorizontalHeaderLabels(headers)
+        self.ui.tabla.setRowCount(len(self.__particulas))
+        row = 0
+        for particula in self.__particulas:
+            self.insertar_particula_en_tabla(particula, row)
+            row += 1
+
     @Slot()
     def action_abrir_archivo(self):
         ubicacion = QFileDialog.getOpenFileName(self, 'Abrir Archivo', '.','*.json')[0]
@@ -52,50 +142,3 @@ class MainWindow(QMainWindow):
                 "Error",
                 "No se pudo crear el archivo " + ubicacion
             )
-
-    @Slot()
-    def click_mostrar(self):
-        self.ui.salida.clear()
-        self.ui.salida.insertPlainText(str(self.__particulas))
-
-    @Slot()
-    def click_agregarFinal(self):
-        iD = self.ui.id_lineEdit.text()
-        velocidad = self.ui.velocidad_lineEdit.text()
-        # Verificamos que las casillas libres no esten vacias y tambien que solo contengan números.
-        if (iD.isnumeric() and velocidad.isnumeric()):
-            origenX = self.ui.origenX_spinBox.value()
-            origenY = self.ui.origenY_spinBox.value()
-            destinoX = self.ui.destinoX_spinBox.value()
-            destinoY = self.ui.destinoY_spinBox.value()
-            red = self.ui.red_spinBox.value()
-            green = self.ui.green_spinBox.value()
-            blue = self.ui.blue_spinBox.value()
-            # Instanciamos una particula para solamente agregarla al final de la lista.
-            particula = Particula(int(iD), origenX, origenY, destinoX, destinoY, int(velocidad), red, green, blue)
-            self.__particulas.agregar_final(particula)
-
-    @Slot()
-    def click_agregarInicio(self):
-        iD = self.ui.id_lineEdit.text()
-        velocidad = self.ui.velocidad_lineEdit.text()
-        # Verificamos que las casillas libres no esten vacias y tambien que solo contengan números.
-        if (iD.isnumeric() and velocidad.isnumeric()):
-            origenX = self.ui.origenX_spinBox.value()
-            origenY = self.ui.origenY_spinBox.value()
-            destinoX = self.ui.destinoX_spinBox.value()
-            destinoY = self.ui.destinoY_spinBox.value()
-            red = self.ui.red_spinBox.value()
-            green = self.ui.green_spinBox.value()
-            blue = self.ui.blue_spinBox.value()
-            # Instanciamos una particula para solamente agregarla al final de la lista.
-            particula = Particula(int(iD), origenX, origenY, destinoX, destinoY, int(velocidad), red, green, blue)
-            self.__particulas.agregar_inicio(particula)
-
-    @Slot()
-    def click_buscar(self):
-        print("buscar")
-
-    @Slot()
-    def click_mostrarTabla(self):
-        print("mostrar tabla")
--- particula.py
+++ particula.py
@@ -1,8 +1,8 @@
 from algoritmos import distancia_euclidiana
 
 class Particula:
-    def __init__(self, iD=0, origen_x=0, origen_y=0, destino_x=0, destino_y=0, velocidad=0, red=0, green=0, blue=0):
-        self.__id = iD
+    def __init__(self, id=0, origen_x=0, origen_y=0, destino_x=0, destino_y=0, velocidad=0, red=0, green=0, blue=0):
+        self.__id = id
         self.__origen_x = origen_x
         self.__origen_y = origen_y
         self.__destino_x = destino_x
@@ -27,7 +27,7 @@ class Particula:
 
     def to_dict(self):
         return {
-            "iD": self.__id,
+            "id": self.__id,
             "origen_x": self.__origen_x,
             "origen_y": self.__origen_y,
             "destino_x": self.__destino_x,
@@ -37,3 +37,45 @@ class Particula:
             "green": self.__green,
             "blue": self.__blue
         }
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def origen_x(self):
+        return self.__origen_x
+
+    @property
+    def origen_y(self):
+        return self.__origen_y
+
+    @property
+    def destino_x(self):
+        return self.__destino_x
+
+    @property
+    def destino_y(self):
+        return self.__destino_y
+
+    @property
+    def velocidad(self):
+        return self.__velocidad
+
+    @property
+    def red(self):
+        return self.__red
+
+    @property
+    def green(self):
+        return self.__green
+
+    @property
+    def blue(self):
+        return self.__blue
+
+    @property
+    def distancia(self):
+        return self.__distancia
+
+    
